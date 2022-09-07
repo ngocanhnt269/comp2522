0bcc5eefa7d9b95aa6d6f0e234614e9ef6725489
ngocanhnt269 <ngocanhnt269@gmail.com>
2022-09-06 21:56:07 -0700

    Anh added the message

---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,8 @@
+package org.comp2522;
+
+public class Main {
+    public static void main(String[] args) {
+        //Print the message
+        System.out.println("Hi everyone! I am Anna");
+    }
+}
