dd6291a4786d94462d63bb97a8e61e58b27abbcd
ngocanhnt269 <ngocanhnt269@gmail.com>
2022-09-06 22:32:38 -0700

    Anh fixed the errors from the checkstyle.

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,8 +1,15 @@
 package org.comp2522;
 
+/**
+ * This is result of the lab01.
+ *
+ * @author Nguyen, Thi Ngoc Anh
+ * @version 1.0
+ *
+ */
 public class Main {
-    public static void main(String[] args) {
-        //Print the message
-        System.out.println("Hi everyone! I am Anna");
-    }
+  public static void main(String[] args) {
+    //Print the message
+    System.out.println("Hi everyone! I am Anna");
+  }
 }
